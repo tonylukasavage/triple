79ad578cc078fea3b7959a06cc376e971d2fceb5
Tony Lukasavage <anthony.lukasavage@gmail.com>
2014-07-15 17:58:59 -0400

    fix echo problem, cleanup

--- app/app.js
+++ app/app.js
@@ -3,12 +3,13 @@ var constants = require('constants');
 var global = this;
 
 var socket = Ti.Network.Socket.createTCP({
-	host: 'localhost', port: 8124,
+	host: 'localhost',
+	port: 8124,
 	connected: function (e) {
 		Ti.Stream.pump(e.socket, readCallback, 1024, true);
-        Ti.Stream.write(e.socket, Ti.createBuffer({
-            value: constants.CONNECT_MESSAGE
-        }), function(){});
+		Ti.Stream.write(e.socket, Ti.createBuffer({
+			value: constants.CONNECT_MESSAGE
+		}), function(){});
 	},
 	error: function (e) {
 		Ti.API.error('Error (' + e.errorCode + '): ' + e.error);
@@ -17,20 +18,17 @@ var socket = Ti.Network.Socket.createTCP({
 socket.connect();
 
 function readCallback(e) {
-    if (e.bytesProcessed == -1)
-    {
-        // Error / EOF on socket. Do any cleanup here.
-        // ...
-    }
-    try {
-        if(e.buffer) {
-            var received = e.buffer.toString();
-            //Ti.API.info('Received: ' + received);
-            eval.call(global, received);
-        } else {
-            Ti.API.error('Error: read callback called with no buffer!');
-        }
-    } catch (ex) {
-        Ti.API.error(ex);
-    }
+	if (e.bytesProcessed === -1) {
+		Ti.API.error('socket error, try again [no bytes]');
+	}
+	try {
+		if(e.buffer) {
+			var received = e.buffer.toString();
+			eval.call(global, received);
+		} else {
+			Ti.API.error('socket error, try again [no buffer]');
+		}
+	} catch (ex) {
+		Ti.API.error(ex);
+	}
 }
