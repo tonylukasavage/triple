7900b5d7dc3d99d2f33fb2edd50eb1c3af4cc2a4
Tony Lukasavage <anthony.lukasavage@gmail.com>
2014-07-23 12:12:52 -0400

    initial placeholder for .clear

--- app/repl.js
+++ app/repl.js
@@ -1,4 +1,5 @@
-var ReplClient = require('ReplClient'),
+var constants = require('constants'),
+	ReplClient = require('ReplClient'),
 	util = require('util');
 
 // establish context for eval
@@ -6,11 +7,15 @@ var global = this;
 
 // create repl client socket
 var client = new ReplClient(function(code) {
-	try {
-		var ret = eval.call(global, code);
-		this.write(util.inspect(ret, { colors: true }));
-	} catch (e) {
-		this.write(util.error(e));
+	if (code === constants.CLEAR_MESSAGE) {
+		this.write(util.error('error: .clear is not yet implemented'));
+	} else {
+		try {
+			var ret = eval.call(global, code);
+			this.write(util.inspect(ret, { colors: true }));
+		} catch (e) {
+			this.write(util.error(e));
+		}
 	}
 });
 
