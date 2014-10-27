c3206411d2c6bc89ddaeede722214a8abda96764
Tony Lukasavage <anthony.lukasavage@gmail.com>
2014-10-27 08:51:39 -0400

    rename to globalContext, fixes #71

--- app/context.js
+++ app/context.js
@@ -1,8 +1,8 @@
-var global = this;
+var globalContext = this;
 
 Ti.App.addEventListener('app:eval', function(e) {
 	try {
-		var value = eval.call(global, e.code);
+		var value = eval.call(globalContext, e.code);
 		Ti.App.fireEvent('app:return', { value: value });
 	} catch (ex) {
 		Ti.App.fireEvent('app:error', {
