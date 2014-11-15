4562fb584ae797bb795fdd39ac175a95d5db8406
Tony Lukasavage <anthony.lukasavage@gmail.com>
2014-11-15 16:57:26 -0500

    fix typo

--- appc.js
+++ appc.js
@@ -1,3 +1,3 @@
 exports.command = function(program) {
-	program.command('repl [file [delay]] [options]', 'REPL for Titanim');
+	program.command('repl [file [delay]] [options]', 'REPL for Titanium');
 };
