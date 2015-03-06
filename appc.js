4cb279723e1d006f59f423b29b4b63429df7ec27
Tony Lukasavage <anthony.lukasavage@gmail.com>
2015-03-06 14:34:12 -0500

    update plugin command description

--- appc.js
+++ appc.js
@@ -1,3 +1,3 @@
 exports.command = function(program) {
-	program.command('repl [file [delay]] [options]', 'REPL for Titanim');
+	program.command('repl [options] [file [delay]]', 'REPL for Titanium');
 };
